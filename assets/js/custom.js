79cd1780891b6ccfc082f88f2adcf19de50b1f0d
Kamil <k.medrala99@gmail.com>
2023-05-09 17:58:28 +0200

    categories autoplay removed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1105,16 +1105,13 @@ $(document).ready(function () {
     slidesPerColumn: 2,
     slidesPerColumnFill: 'row',
     watchOverflow: true,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
     breakpoints: {
       560: {
         slidesPerView: 2,
         slidesPerGroup: 1,
         slidesPerColumn: 2,
         slidesPerColumnFill: 'row',
+        allowTouchMove: true,
       },
       992: {
         slidesPerView: 3,
